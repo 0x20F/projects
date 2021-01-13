c4aa973c8e2b05381fbe8892809b8dfdd4464873
Poly <alexhexan22@gmail.com>
2021-01-13 20:48:20 +0100

    Update image urls

--- src/app/data.js
+++ src/app/data.js
@@ -16,71 +16,71 @@ export const data = {
 
     bunny: {
         url: 'https://gitlab.com/0x303/bunny',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/19738053/005_-_SwZzbk6.png'
+        img: 'https://imgur.com/QFc2OWs.png'
     },
 
     jenkins: {
         url: 'https://gitlab.com/0x303/jenkins',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/19538102/chun-lo-demon-patreon__1_-min__1_.jpg'
+        img: 'https://imgur.com/AilvoBM.jpg'
     },
 
     ccid: {
         url: 'https://gitlab.com/0x303/ccid',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/19538306/antonio-ferrer-closeup-thumbnail.jpg'
+        img: 'https://imgur.com/z3oijWr.jpg'
     },
 
     stash: {
         url: 'https://gitlab.com/0x303/stash',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/19538120/psdelux-20170820-skull-steps-psdelux-0000-layer-1__1_.jpg'
+        img: 'https://imgur.com/eteD7su.jpg'
     },
 
     default: {
         url: 'https://gitlab.com/0x303/default',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18610177/015_-_gYy04Wa.jpg'
+        img: 'https://imgur.com/4mg7H6i.jpg'
     },
 
     machine: {
         url: 'https://gitlab.com/0x303/machine',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18828755/013_-_wsG3I97.jpg'
+        img: 'https://imgur.com/JNnjIB5.jpg'
     },
 
     break: {
         url: 'https://gitlab.com/0x303/break',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18961717/tyler-smith-comsketch04.jpg'
+        img: 'https://imgur.com/gmcsePE.jpg'
     },
 
     recon: {
         url: 'https://gitlab.com/0x303/recon',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18733785/020_-_Ykfhlop.png'
+        img: 'https://imgur.com/XcOd2Ud.jpg'
     },
 
     template: {
         url: 'https://gitlab.com/0x303/template',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18610761/075_-_4rI0bt0.png'
+        img: 'https://imgur.com/cxTCNVN.jpg'
     },
 
     exploit: {
         url: 'https://gitlab.com/0x303/exploit',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18919636/hristian-ivanov-shyne-cover.jpg'
+        img: 'https://imgur.com/uuJJt1Y.jpg'
     },
 
     irc: {
         url: 'https://gitlab.com/0x303/irc',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18933441/rafael-amarante-art2.jpg'
+        img: 'https://imgur.com/nwklZs3.jpg'
     },
 
     info: {
         url: 'https://gitlab.com/0x303/info',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18565703/054_-_0U0iXN0.png'
+        img: 'https://imgur.com/kHaGAgT.jpg'
     },
 
     android: {
         url: 'https://gitlab.com/0x303/android',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18565382/jean-sebastien-rolhion-skelton.jpg'
+        img: 'https://imgur.com/HRoTprM.jpg'
     },
 
     wordlists: {
         url: 'https://gitlab.com/0x303/wordlists',
-        img: 'https://gitlab.com/uploads/-/system/project/avatar/18831081/091_-_PRdZe1d.png'
+        img: 'https://imgur.com/pgWiXKv.jpg'
     }
 };
