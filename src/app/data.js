a1bdf4347f77c0ef47608bd46cb977374ffcb594
0x20F <10909659+0x20F@users.noreply.github.com>
2021-01-16 13:03:07 +0000

    Deploying to pages from  @ 64e21df7ca4774bb862a4883fbece57f286a03f8 🚀

--- src/app/data.js
+++ src/app/data.js
@@ -2,91 +2,112 @@ export const data = {
     io: {
         url: 'https://github.com/0x20f/io',
         img: 'https://cdna.artstation.com/p/assets/images/images/007/219/320/large/florian-d-k-s-allblack1sta2.jpg',
-        showcase: true
+        showcase: true,
+        description: `Giving everyone the ability to build and code cool things by making it easy.
+        IO is a visual programming tool that allows you to create, edit, and manipulate text
+        and configuration files through a node graph.`,
+        img_author: 'https://www.artstation.com/artwork/4g4b2'
     },
 
     pepe: {
         url: 'https://github.com/0x20f/pepe',
-        img: 'https://imgur.com/F2HgdYV.jpg'
+        img: 'https://imgur.com/F2HgdYV.jpg',
+        description: ''
     },
 
     paris: {
         url: 'https://github.com/0x20f/paris',
-        img: 'https://imgur.com/ylNGH6k.jpg'
+        img: 'https://imgur.com/ylNGH6k.jpg',
+        description: ''
     },
 
     xtype: {
         url: 'https://github.com/0x20F/xtype',
-        img: 'https://imgur.com/ROAJJ4L.jpg'
+        img: 'https://imgur.com/ROAJJ4L.jpg',
+        description: ''
     },
 
     bunny: {
         url: 'https://gitlab.com/0x303/bunny',
-        img: 'https://imgur.com/QFc2OWs.png'
+        img: 'https://imgur.com/QFc2OWs.png',
+        description: ''
     },
 
     jenkins: {
         url: 'https://gitlab.com/0x303/jenkins',
-        img: 'https://imgur.com/AilvoBM.jpg'
+        img: 'https://imgur.com/AilvoBM.jpg',
+        description: ''
     },
 
     ccid: {
         url: 'https://gitlab.com/0x303/ccid',
-        img: 'https://imgur.com/z3oijWr.jpg'
+        img: 'https://imgur.com/z3oijWr.jpg',
+        description: ''
     },
 
     stash: {
         url: 'https://gitlab.com/0x303/stash',
-        img: 'https://imgur.com/eteD7su.jpg'
+        img: 'https://imgur.com/eteD7su.jpg',
+        description: ''
     },
 
     default: {
         url: 'https://gitlab.com/0x303/default',
-        img: 'https://imgur.com/4mg7H6i.jpg'
+        img: 'https://imgur.com/4mg7H6i.jpg',
+        description: ''
     },
 
     machine: {
         url: 'https://gitlab.com/0x303/machine',
-        img: 'https://imgur.com/JNnjIB5.jpg'
+        img: 'https://imgur.com/JNnjIB5.jpg',
+        description: ''
     },
 
     break: {
         url: 'https://gitlab.com/0x303/break',
-        img: 'https://imgur.com/gmcsePE.jpg'
+        img: 'https://imgur.com/gmcsePE.jpg',
+        description: ''
     },
 
     recon: {
         url: 'https://gitlab.com/0x303/recon',
-        img: 'https://imgur.com/XcOd2Ud.jpg'
+        img: 'https://imgur.com/XcOd2Ud.jpg',
+        description: ''
     },
 
     template: {
         url: 'https://gitlab.com/0x303/template',
-        img: 'https://imgur.com/cxTCNVN.jpg'
+        img: 'https://imgur.com/cxTCNVN.jpg',
+        description: ''
     },
 
     exploit: {
         url: 'https://gitlab.com/0x303/exploit',
-        img: 'https://imgur.com/uuJJt1Y.jpg'
+        img: 'https://imgur.com/uuJJt1Y.jpg',
+        description: ''
     },
 
     irc: {
         url: 'https://gitlab.com/0x303/irc',
-        img: 'https://imgur.com/nwklZs3.jpg'
+        img: 'https://imgur.com/nwklZs3.jpg',
+        description: ''
     },
 
     info: {
         url: 'https://gitlab.com/0x303/info',
-        img: 'https://imgur.com/kHaGAgT.jpg'
+        img: 'https://imgur.com/kHaGAgT.jpg',
+        description: ''
     },
 
     android: {
         url: 'https://gitlab.com/0x303/android',
-        img: 'https://imgur.com/HRoTprM.jpg'
+        img: 'https://imgur.com/HRoTprM.jpg',
+        description: ''
     },
 
     wordlists: {
         url: 'https://gitlab.com/0x303/wordlists',
-        img: 'https://imgur.com/pgWiXKv.jpg'
+        img: 'https://imgur.com/pgWiXKv.jpg',
+        description: ''
     }
 };
