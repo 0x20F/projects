bf2f677ff06afaa7e048e40039f72a5e79955ce3
Poly <10909659+0x20F@users.noreply.github.com>
2021-01-21 13:35:22 +0000

    Get it closer to what I want

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -51,15 +51,41 @@ export default class App extends Component {
 
         // Render graph
         let options = {
+            grid: {
+                show: false
+            },
+            legend: {
+                show: false
+            },
             chart: {
-                type: 'line'
+                type: 'bar',
+                animations: {
+                    enabled: false
+                }
             },
             series: [{
                 name: 'sales',
                 data: [30,40,35,50,49,60,70,91,125]
             }],
             xaxis: {
-                categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+                show: false,
+                labels: {
+                    show: false
+                },
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false
+                }
+            },
+            yaxis: {
+                show: false
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true
+                }
             }
         }
         
